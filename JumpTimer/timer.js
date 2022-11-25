18e25668c8bc5d62fa0bbbc430e8167bccc9c357
GekkePerp <95859662+GekkePerp@users.noreply.github.com>
2022-11-25 11:26:41 +0100

    timers

--- JumpTimer/timer.js
+++ JumpTimer/timer.js
@@ -21,6 +21,7 @@ secs %= 60;
 let pretty =( (mins < 10) ? "0" : "" ) + mins + ":" + ( (secs < 10) ? "0" : "" ) + secs;
 
 document.getElementById("countdown").innerHTML = pretty;
+document.getElementById("countdown2").innerHTML = pretty;
 }
 
 startTimer(60*60);
--- JumpTimer/timer2.js
+++ JumpTimer/timer2.js
@@ -3,10 +3,10 @@ let ticker2;
 
 function startTimer(secs2) {
 timeInSecs2 = parseInt(secs2);
-ticker2 = setInterval("tick()", 1000); 
+ticker2 = setInterval("tick2()", 1000); 
 }
 
-function tick( ) {
+function tick2( ) {
 let secs2 = timeInSecs2;
 if (secs2 > 0) {
 timeInSecs2--; 
@@ -20,7 +20,7 @@ let mins2 = Math.floor(secs2/60);
 secs2 %= 60;
 let pretty2 =( (mins2 < 10) ? "0" : "" ) + mins2 + ":" + ( (secs2 < 10) ? "0" : "" ) + secs2;
 
-document.getElementById("countdown2").innerHTML = pretty2;
+document.getElementById("countdown3").innerHTML = pretty2;
 }
 
 startTimer(30*60);
