c88e935f536698cf7a881d06cd4c590aa5ff87a5
GekkePerp <95859662+GekkePerp@users.noreply.github.com>
2022-12-14 11:12:48 +0100

    countdown van 10

--- JumpTimer/timer2.js
+++ JumpTimer/timer2.js
@@ -22,9 +22,27 @@ let pretty2 =( (mins2 < 10) ? "0" : "" ) + mins2 + ":" + ( (secs2 < 10) ? "0" : 
 
 document.getElementById("countdown3").innerHTML = pretty2;
 document.getElementById("countdown").innerHTML = pretty2;
-}
+document.getElementById("fullscreenTimerId").innerHTML = secs2;
 
-startTimer(30*60);
+// countdown van 10
+
+if(mins2 == 0 && secs2 < 11){
+    document.body.style.backgroundColor = 'lime';
+    document.getElementById("container").style.display="none"
+    document.getElementById("fullscreenTimerId").style.display="block"
+}
+ setTimeout(function delay(){
+ if(secs2 == 0){
+    document.body.style.backgroundColor = '#00bfff';
+    document.getElementById("container").style.display="grid"
+    document.getElementById("fullscreenTimerId").style.display="none"
+}
+}, 1000);
+}
+        
+startTimer(0.2*60);
+
+
 
 
 
