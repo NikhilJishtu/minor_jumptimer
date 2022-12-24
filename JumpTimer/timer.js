ecfcfe8260afda58805667152f5b0fd57112489b
GekkePerp <95859662+GekkePerp@users.noreply.github.com>
2022-12-24 21:52:48 +0100

    looooool

--- JumpTimer/timer.js
+++ JumpTimer/timer.js
@@ -1,56 +1,104 @@
-let timeInSecs;
-let ticker;
+let timeInSecs2;
+let ticker2;
 
-const targetTime = new Date();
+  const targetTime = new Date();
 
-targetTime.setHours(11, 0, 0, 0);
+  targetTime.setHours(11, 0, 0, 0);
 
-function startTimer() {
-  // Calculate the time remaining in seconds
-  timeInSecs = Math.floor((targetTime - Date.now()) / 1000);
+  function startTimer() {
+    // Calculate the time remaining in seconds
+    timeInSecs2 = Math.floor((targetTime - Date.now()) / 1000);
 
-  // If the target time has passed, set the target time to the next day
-  if (timeInSecs < 0) {
-    targetTime.setDate(targetTime.getDate() + 1);
-    timeInSecs = Math.floor((targetTime - Date.now()) / 1000);
+    // If the target time has passed, set the target time to the next day
+    if (timeInSecs2 < 0) {
+      targetTime.setDate(targetTime.getDate() + 1);
+      timeInSecs2 = Math.floor((targetTime - Date.now()) / 1000);
+    }
+
+    ticker2 = setInterval(tick2, 1000);
   }
 
-  ticker = setInterval(tick, 1000);
+  function tick2() {
+    // Decrease the time remaining by 1 second
+    timeInSecs2--;
+  
+    // Calculate the time remaining in seconds
+    let secs2 = timeInSecs2;
+    
+    // If the target time has passed, set the target time to the next day
+    if (secs2 <= 0) {
+      targetTime.setDate(targetTime.getDate() + 1);
+      timeInSecs2 = Math.floor((targetTime - Date.now()) / 1000);
+      secs2 = timeInSecs2;
+    }
+  
+    // Calculate the time remaining in half-hour blocks
+    let halfHourBlocks = Math.floor(secs2 / 3600);
+    secs2 %= 3600;
+  
+    // Calculate the time remaining in minutes and seconds
+    let mins2 = Math.floor(secs2 / 60);
+    secs2 %= 60;
+  
+    // Format the time as a string
+    let pretty2 =  (mins2 < 10 ? "0" : "") + mins2 + ":" + (secs2 < 10 ? "0" : "") + secs2;
+  
+    // Update the countdown element
+    document.getElementById("countdown2").innerHTML = pretty2;
+    document.getElementById("fullscreenTimerId").innerHTML = secs2;
+  
+    // countdown van 10
+    if (mins2 == 0 && secs2 < 11) {
+      document.body.style.backgroundColor = "#2803a0";
+      document.getElementById("container").style.display = "none";
+      document.getElementById("fullscreenTimerId").style.display = "block";
 }
-
-function tick() {
-  // Decrease the time remaining by 1 second
-  timeInSecs--;
-
-  // Calculate the time remaining in seconds
-  let secs = timeInSecs;
-
-  // If the target time has passed, set the target time to the next day
-  if (secs <= 0) {
-    targetTime.setDate(targetTime.getDate() + 1);
-    timeInSecs = Math.floor((targetTime - Date.now()) / 1000);
-    secs = timeInSecs;
-  }
-
-  // Calculate the time remaining in half-hour blocks
-  let halfHourBlocks = Math.floor(secs / 1800);
-  secs %= 1800;
-
-  // Calculate the time remaining in minutes and seconds
-  let mins = Math.floor(secs / 60);
-  secs %= 60;
-
-  // If the timer has reached 0, reset it to 60 minutes
-  if (halfHourBlocks === 0 && secs === 0) {
-    timeInSecs = 3600;
-    mins = 60;
-  }
-
-  // Format the time as a string
-  let pretty =  (mins < 10 ? "0" : "") + mins + ":" + (secs < 10 ? "0" : "") + secs;
-
-  // Update the countdown element
-  document.getElementById("countdown2").innerHTML = pretty;
+setTimeout(function delay() {
+if (secs2 == 0) {
+document.body.style.backgroundColor = "#00bfff";
+document.getElementById("container").style.display = "grid";
+document.getElementById("fullscreenTimerId").style. display = "none";
+}
+}, 1000);
 }
 
 startTimer();
+
+
+
+
+
+
+
+// let timeInSecs;
+// let ticker;
+
+// function startTimer(secs) {
+//   timeInSecs = parseInt(secs);
+//   ticker = setInterval("tick()", 1000); 
+// }
+
+// function tick( ) {
+//   let secs = timeInSecs;
+//   if (secs > 0) {
+//     timeInSecs--; 
+//   }
+//   else {
+//     clearInterval(ticker);
+//     startTimer(3600); // reset timer to 60 minutes
+//   }
+
+//   // If the timer hits 30 minutes, reset it to 60 minutes
+//   if (secs == 1800) {
+//     clearInterval(ticker);
+//     startTimer(3600);
+//   }
+
+//   let mins = Math.floor(secs/60);
+//   secs %= 60;
+//   let pretty = ( (mins < 10) ? "0" : "" ) + mins + ":" + ( (secs < 10) ? "0" : "" ) + secs;
+
+//   document.getElementById("countdown2").innerHTML = pretty;
+// }
+
+// startTimer(3600); 
