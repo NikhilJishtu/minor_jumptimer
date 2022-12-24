a46740a1300266bfbea0048cac29c3b42647a3aa
GekkePerp <95859662+GekkePerp@users.noreply.github.com>
2022-12-24 13:01:07 +0100

    s

--- JumpTimer/timer.js
+++ JumpTimer/timer.js
@@ -2,12 +2,18 @@ let timeInSecs;
 let ticker;
 
 const targetTime = new Date();
-// Set the target time to the next half-hour mark
-targetTime.setMinutes(targetTime.getMinutes() + 30 - (targetTime.getMinutes() % 30), 0, 0);
+
+targetTime.setHours(11, 0, 0, 0);
 
 function startTimer() {
   // Calculate the time remaining in seconds
   timeInSecs = Math.floor((targetTime - Date.now()) / 1000);
+
+  // If the target time has passed, set the target time to the next day
+  if (timeInSecs < 0) {
+    targetTime.setDate(targetTime.getDate() + 1);
+    timeInSecs = Math.floor((targetTime - Date.now()) / 1000);
+  }
 
   ticker = setInterval(tick, 1000);
 }
@@ -19,37 +25,32 @@ function tick() {
   // Calculate the time remaining in seconds
   let secs = timeInSecs;
 
-  // If the target time has passed, set the target time to the next half-hour mark
+  // If the target time has passed, set the target time to the next day
   if (secs <= 0) {
-    targetTime.setMinutes(targetTime.getMinutes() + 30);
+    targetTime.setDate(targetTime.getDate() + 1);
     timeInSecs = Math.floor((targetTime - Date.now()) / 1000);
     secs = timeInSecs;
   }
 
+  // Calculate the time remaining in half-hour blocks
+  let halfHourBlocks = Math.floor(secs / 1800);
+  secs %= 1800;
+
   // Calculate the time remaining in minutes and seconds
   let mins = Math.floor(secs / 60);
   secs %= 60;
+
+  // If the timer has reached 0, reset it to 60 minutes
+  if (halfHourBlocks === 0 && secs === 0) {
+    timeInSecs = 3600;
+    mins = 60;
+  }
 
   // Format the time as a string
   let pretty =  (mins < 10 ? "0" : "") + mins + ":" + (secs < 10 ? "0" : "") + secs;
 
   // Update the countdown element
   document.getElementById("countdown2").innerHTML = pretty;
-  document.getElementById("fullscreenTimerId").innerHTML = secs;
-
-  // countdown van 10
-  if (mins == 0 && secs < 11) {
-    document.body.style.backgroundColor = "#2803a0";
-    document.getElementById("container").style.display = "none";
-    document.getElementById("fullscreenTimerId").style.display = "block";
-  }
-  setTimeout(function delay() {
-    if (secs == 0) {
-      document.body.style.backgroundColor = "#00bfff";
-      document.getElementById("container").style.display = "grid";
-      document.getElementById("fullscreenTimerId").style.display = "none";
-    }
-  }, 1000);
 }
 
 startTimer();
